e8fe26d92716073108b4b27978e11daeadb3ad6e
Julian Angel Gallego <julian.angelg@udea.edu.co>
2021-01-25 19:15:39 -0500

    movement 0.1

--- src/app/components/user/movement/movement.component.ts
+++ src/app/components/user/movement/movement.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-movement',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovementComponent implements OnInit {
 
-  constructor() { }
+  contactForm:FormGroup;
+ 
+  /*categories = [
+    { id_categoria: 1, nombre_categoria: "United States" },
+    { id_categoria: 2, nombre_categoria: "Australia" },
+    { id_categoria: 3, nombre_categoria: "Canada" },
+    { id_categoria: 4, nombre_categoria: "Brazil" },
+    { id_categoria: 5, nombre_categoria: "England" }
+  ];*/
+
+  categories  = [];
+  concepts: any;
+  products: any;
+  adquisitionPoints: any;
+  constructor( private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.contactForm = this.fb.group({
+      category: [null]
+    });
+  }
+
+  submit() {
+    console.log("Form Submitted")
+    console.log(this.contactForm.value)
   }
 
 }
