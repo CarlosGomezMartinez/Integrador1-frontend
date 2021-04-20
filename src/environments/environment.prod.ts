4a2a8a60585a835877872e4d8ff33d4d35b40f71
Julian Angel Gallego <julian.angelg@udea.edu.co>
2021-04-20 12:28:28 -0500

    enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBaehFZKN07SyLxGAdDWCke5da8LNBpU_A",
+    authDomain: "login-control-gastos.firebaseapp.com",
+    databaseURL: "https://login-control-gastos.firebaseio.com",
+    projectId: "login-control-gastos",
+    storageBucket: "login-control-gastos.appspot.com",
+    messagingSenderId: "720560560456",
+    appId: "1:720560560456:web:089a22c683453d466c7b7d"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig : {
     apiKey: "AIzaSyBaehFZKN07SyLxGAdDWCke5da8LNBpU_A",
     authDomain: "login-control-gastos.firebaseapp.com",
